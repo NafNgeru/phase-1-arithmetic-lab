b1b8c2b1319f74af35d3f3114202bf986de386d6
Naftaly Ngeru <n.ngeru.k@gmail.com>
2024-12-02 11:45:01 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2;
+console.log(multiply);
+
+let random = Math.ceil(Math.random() * 10);
+console.log(random)
+
+let num3 = 32
+let num4 = 7
+let mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(0, 5, 10, 15, 20)
+console.log(max);
+
